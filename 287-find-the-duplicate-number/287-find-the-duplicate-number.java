d58a54c9ddf1389f93af73df8fc198fb46fd9ab0
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-05-08 22:07:07 +0900

    Time: 5 ms (81.46%), Space: 75.7 MB (51.74%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,19 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // nums[nums[i]] <- -1 * nums[nums[i]] : nums에 nums[i] 가 있는 경우
-        // => nums[i] <0 : i 가 nums 배열에 존재
-        int num;
-        for (int i = 0; i < nums.length; i++) {
-            num = nums[i] > 0 ? nums[i] : -nums[i];
-            if (nums[num] < 0) {
-                // num 을 이미 방문한 경우
-                return num;
-            }
+        int tortoise = nums[0];
+        int hare = nums[0];
 
-            nums[num] = -nums[num];
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+        
+        // 사이클의 진입점이 중복된 숫자찾기
+        // Key Idea : i=k*L >= N for some k <=> f_i = f_(2i) where N : 시작점, L : 사이클 길이
+        // => 현재 거북이가 x칸 이동한 경우, 현재 토끼는 2*x칸 이동 where x=k*L
+        // => 토끼를 0칸으로 복구한 후, N칸 움직이면 토끼=f(N), 거북이=f(N + k*L)=f(N)
+        // => 토끼를 첫시작점으로 초기화하고 토끼와 거북이를 한칸씩 옮기다가 일치하는 부분이 진입점
+        hare = nums[0];
+        while(tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-
-        // never reach
-        return -1;
+        return hare;
     }
 }
