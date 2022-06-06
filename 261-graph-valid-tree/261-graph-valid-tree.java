bf07415688e53f67066c9f4026b2e24c92d100d2
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-06 14:45:13 +0900

    Time: 7 ms (29.81%), Space: 47.5 MB (28.32%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,34 +1,42 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        List<List<Integer>> graph = new ArrayList<>();
+        // Make adjacent graph
+        List<List<Integer>> graph = new ArrayList<>(n);
         for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+
         for (int[] e : edges) {
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
 
-        Map<Integer, Integer> parent = new HashMap<>();
-        Stack<Integer> dfs = new Stack<>();
-        dfs.add(0);
-        parent.put(0, -1);
+        boolean[] isVisited = new boolean[n];
 
-        int cur;
-        while (!dfs.isEmpty()) {
-            cur = dfs.pop();
+        // BFS : (node, parent)
+        Queue<int[]> bfs = new ArrayDeque<>();
+        bfs.add(new int[]{0, -1});
+        isVisited[0] = true;
 
-            for (int next : graph.get(cur)) {
-                // 부모인 경우는 무시
-                if(parent.get(cur) == next) continue;
+        int[] cur;
+        while (!bfs.isEmpty()) {
+            cur = bfs.poll();
 
-                // 이미 지나간 노드인 경우 거짓
-                if(parent.containsKey(next)) return false;
+            for (int next : graph.get(cur[0])) {
+                // 부모 노드는 무시
+                if (next == cur[1]) continue;
 
-                parent.put(next, cur);
-                dfs.push(next);
+                // 사이클 형성
+                if (isVisited[next]) return false;
+
+                isVisited[next] = true;
+                bfs.add(new int[]{next, cur[0]});
             }
         }
 
-        // 모두 연결되어 있어야만 트리
-        return parent.size() == n;
+
+        // 컴포넌트가 1개인지 확인
+        for (int i = 0; i < n; i++) {
+            if (!isVisited[i]) return false;
+        }
+        return true;
     }
 }
