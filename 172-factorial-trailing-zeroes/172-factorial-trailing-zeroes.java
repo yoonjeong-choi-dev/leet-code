978717a3d197244ba676160277af1f86b399b332
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-08 16:57:40 +0900

    Time: 7 ms (15.32%), Space: 41 MB (56.14%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -1,21 +1,17 @@
 class Solution {
     public int trailingZeroes(int n) {
-        // n! 의 trailing zeroes == 소인수분해 시 2와 5의 개수
-        int numTwo = 0, numFive = 0;
+        // 2의 개수보다 5의 개수가 항상 많음
+        // => 5의 배수들에 대해서만 탐색하면 됨
+        int numFive = 0;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 5; i <= n; i += 5) {
             int num = i;
-            while (num % 2 == 0) {
-                numTwo++;
-                num /= 2;
-            }
-
             while (num % 5 == 0) {
                 numFive++;
                 num /= 5;
             }
         }
 
-        return Math.min(numTwo, numFive);
+        return numFive;
     }
 }
