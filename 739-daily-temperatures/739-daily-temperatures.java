e0a44455239126fdc11654bfbc0e048f64b6eebe
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-12 09:00:22 +0900

    Time: 130 ms (68.60%), Space: 54.4 MB (86.12%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -3,27 +3,19 @@ class Solution {
         int size = temperatures.length;
         int[] ans = new int[size];
 
-        // i ~ size-1 중에 가장 높은 온도
-        int curMax = Integer.MIN_VALUE;
-        int curTemp, days;
+        // 온도에 대한 내림차순 스택
+        Stack<Integer> monotonic = new Stack<>();
 
-        // 이후에 따뜻해지는 날을 찾아야 하므로 거꾸로 계산
-        for (int i = size - 1; i >= 0; i--) {
-            curTemp = temperatures[i];
+        for (int curDay = 0; curDay < size; curDay++) {
+            int curTemper = temperatures[curDay];
 
-            // 현재 온도가 i ~ size-1 중에 가장 높은 온도
-            // => i 온도는 따뜻해질 가능성 X
-            if (curMax <= curTemp) {
-                curMax = curTemp;
-                continue;
+            // 현재 온도보다 낮은 이전 온도 날짜 업데이트 
+            while (!monotonic.isEmpty() && temperatures[monotonic.peek()] < curTemper) {
+                int prevDay = monotonic.pop();
+                ans[prevDay] = curDay - prevDay;
             }
 
-            days = 1;
-            while (temperatures[i + days] <= curTemp) {
-                // 현재 온도보다 따뜻한 최소 날짜 찾기
-                days += ans[i + days];
-            }
-            ans[i] = days;
+            monotonic.push(curDay);
         }
         return ans;
     }
