8fa01e83b0ee788e8ce6e7e8408a512c0327a0f1
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-15 14:54:06 +0900

    Time: 87 ms (45.58%), Space: 166.9 MB (11.81%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,4 +1,4 @@
-class Node {
+    class Node {
         int key;
         int value;
         Node prev;
@@ -16,7 +16,7 @@ class Node {
 
         private final int capacity;
         private final Map<Integer, Node> cache;
-        private Node head, tail;
+        private final Node head, tail;
 
         public LRUCache(int capacity) {
             this.capacity = capacity;
@@ -34,17 +34,8 @@ class Node {
             if (node == null) return -1;
 
             // 현재 노드를 가장 앞으로 이동
-            // 1) node 전후 노드 연결
-            Node next = node.next;
-            Node prev = node.prev;
-            prev.next = next;
-            next.prev = prev;
-
-            // 2) node 헤드로 이동
-            node.prev = head;
-            node.next = head.next;
-            head.next.prev = node;
-            head.next = node;
+            removeNode(node);
+            addFirst(node);
 
             return node.value;
         }
@@ -57,40 +48,40 @@ class Node {
                 node.value = value;
 
                 // 캐시 업데이트
-                Node next = node.next;
-                Node prev = node.prev;
-                prev.next = next;
-                next.prev = prev;
-
-                node.prev = head;
-                node.next = head.next;
-                head.next.prev = node;
-                head.next = node;
-
+                removeNode(node);
+                addFirst(node);
             } else {
                 // 캐시에 없는 경우 => 노드 추가
                 node = new Node(key, value);
 
                 // 캐시 용량이 부족한 경우에는 가장 뒷부분 삭제
-                if(cache.size() == capacity) {
+                if (cache.size() == capacity) {
                     // 캐시에서 가장 뒷 부분 삭제
                     Node toDelete = tail.prev;
-                    Node prev = toDelete.prev;
-                    prev.next = tail;
-                    tail.prev = prev;
+                    removeNode(toDelete);
 
                     cache.remove(toDelete.key);
                 }
 
                 // 노두 추가
                 cache.put(key, node);
-
-                node.prev = head;
-                node.next = head.next;
-                head.next.prev = node;
-                head.next = node;
-
+                addFirst(node);
             }
+        }
+
+        private void removeNode(Node node) {
+            Node prev = node.prev;
+            Node next = node.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+
+        private void addFirst(Node node) {
+            node.prev = head;
+            node.next = head.next;
+
+            head.next.prev = node;
+            head.next = node;
         }
     }
 /**
