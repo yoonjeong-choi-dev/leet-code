4526e9ffa37495eaad10610d1bfae9e478af3413
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-15 21:38:48 +0900

    Time: 28 ms (51.47%), Space: 44.6 MB (72.79%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,38 +1,37 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        // Edge Case
         if (amount == 0) return 0;
-
+        
         // 작은 돈부터 고려
         Arrays.sort(coins);
 
-        // dp[i] : i원을 낼 수 있는 경우의 수
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, -1);
+        for (int coin : coins) {
+            if(coin > amount) break;
+            dp[coin] = 1;
+        }
 
+        // 각 돈에 대한 경우의 수 계산
         for (int pay = 1; pay <= amount; pay++) {
+            if (dp[pay] != -1) continue;
+
             for (int coin : coins) {
+                if (pay < coin) break;
 
-                // pay를 거스르기 위한 coin 내기
-                if (pay < coin) continue;
-                if (pay == coin) {
-                    // 오름차순 정렬을 했기 때문에 이후 코인의 값은 현재 값보다 큼
-                    dp[pay] = 1;
-                    break;
-                }
-
-                // pay-coin 의 경우의 수 + 1(현재 코인 내기)
+                // coin 을 내는 경우 경우의 수
                 if (dp[pay - coin] != -1) {
-                    if(dp[pay] == -1) {
-                        // 처음 찾은 정답
+                    if (dp[pay] == -1) {
+                        // 첫 발견
                         dp[pay] = dp[pay - coin] + 1;
                     } else {
-                        // 두번째 이후 찾은 정답은 최소값
+                        // 이후 
                         dp[pay] = Math.min(dp[pay], dp[pay - coin] + 1);
                     }
                 }
             }
         }
-
 
         return dp[amount];
     }
