d885a5214f729eee38d0820aafadfa28ea5de17c
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-15 17:28:55 +0900

    Time: 22 ms (47.69%), Space: 47.7 MB (23.35%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n == 0) return false;
+                // int 형으로 표현할 수 있는 최대 3의 제곱수 구하기
+//        int t = 3;
+//        int step = 1;
+//        while (t <= Integer.MAX_VALUE / 3) {
+//            step++;
+//            t *= 3;
+//        }
+//
+//        System.out.println(step); // 19
+//        System.out.println(t);    // 1162261467
 
-        while (n != 1) {
-            if (n % 3 != 0) return false;
-            n = n / 3;
-        }
-
-        return true;
+        return n > 0 && 1162261467 % n == 0;
     }
 }
