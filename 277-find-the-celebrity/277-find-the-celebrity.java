9d57ed0c9cc507ae1a3a1389558443b3d9a3ebfd
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-15 21:32:12 +0900

    Time: 17 ms (87.41%), Space: 42.5 MB (87.57%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -3,34 +3,29 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
-        // inDegree[i] : i를 아는 사람 수
-        int[] inDegree = new int[n];
-
-        // outDegree[i] : i가 아는 사람 수
-        int[] outDegree = new int[n];
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (knows(i, j)) {
-                    // i가 j를 아는 경우
-                    inDegree[j]++;
-                    outDegree[i]++;
-                }
-
-                if (knows(j, i)) {
-                    // j가 i를 아는 경우
-                    inDegree[i]++;
-                    outDegree[j]++;
-                }
+        // Idea : i가 j를 아는 경우 => i는 정답이 될 수 없고, j는 후보
+        // Condition : There will be exactly one celebrity if they are at the party.
+        // => 0 ~ n-1 까지 순회하면서 후보를 찾는다
+        int candidate = 0;
+        for (int i = 1; i < n; i++) {
+            // candidate < i
+            if (knows(candidate, i)) {
+                // candidate 이 i를 아는 경우 => i가 후보
+                // => candidate 보다 작은 값의 사람은 후보가 될 수 없음
+                candidate = i;
             }
         }
 
-        // celeb == x <=> inDegree[x] = n-1 && outDegree[x] == 0
+        // candidate 이 유명한지 확인
+        boolean isCeleb = true;
         for (int i = 0; i < n; i++) {
-            if (outDegree[i] == 0 && inDegree[i] == n - 1) return i;
+            if (i == candidate) continue;
+            if (knows(candidate, i) || !knows(i, candidate)) {
+                isCeleb = false;
+                break;
+            }
         }
 
-        // 없는 경우 -1
-        return -1;
+        return isCeleb ? candidate : -1;
     }
 }
