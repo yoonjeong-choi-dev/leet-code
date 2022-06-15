dfe68a810777f1c35cc6413de5e052829b905dae
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-15 20:21:15 +0900

    Time: 5 ms (86.52%), Space: 97.1 MB (63.24%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,19 +10,43 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
-        }
+        // Assumption : size >=2
+        if (head == null || head.next == null) return true;
 
-        int left = 0, right = list.size() - 1;
-        while (left < right) {
-            if (!list.get(left).equals(list.get(right))) return false;
-            left++;
-            right--;
+        // Step 1 : 리스트를 두개로 나누기 => (n,n) or (n+1,n)
+        ListNode first = head, second = head;
+        while (second.next != null && second.next.next != null) {
+            first = first.next;
+            second = second.next.next;
         }
+        second = first.next;
+        first.next = null;
+        first = head;
 
+        // Step 2 : 두번째 리스트 반전
+        // prev(null) -> node1(cur) -> node2 -> node3 -> node4
+        // null <- node1(next prev), node2(next cur) -> node3 -> node4
+        // null <- node1 <- node2(next prev), node3(next cur) -> node4
+        ListNode prev = null, cur = second, next;
+        while (cur != null) {
+            // 다음 노드 임시 저장 : 현재 노드의 next 를 prev 노드에 연결하기 위해
+            next = cur.next;
+
+            // prev->cur 을 prev<-cur 로 변경
+            cur.next = prev;
+
+            // prev 및 cur 노드 변경
+            prev = cur;
+            cur = next;
+        }
+        second = prev;
+
+        // compare first and second
+        while (second != null) {
+            if (first.val != second.val) return false;
+            first = first.next;
+            second = second.next;
+        }
 
         return true;
     }
