24a0eb2e7f96cdf48b3dd67033101eb62e2c5b69
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-16 03:27:18 +0900

    Time: 1 ms (86.91%), Space: 49.4 MB (32.52%) - LeetHub

--- 280-wiggle-sort/280-wiggle-sort.java
+++ 280-wiggle-sort/280-wiggle-sort.java
@@ -1,16 +1,19 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        // 오름차 순으로 정렬
-        Arrays.sort(nums);
-
-        // 조건 : nums[0] <= nums[1] >= nums[2] <= nums[3]...
-        // => swap(2i+1, 2i+2), i>=0
-        int upperBound = nums.length - 1;
+        // 조건 : nums[0] <= nums[1]
+        boolean prevIncrease = false;
         int temp;
-        for (int i = 1; i < upperBound; i += 2) {
-            temp = nums[i];
-            nums[i] = nums[i + 1];
-            nums[i + 1] = temp;
+        for (int i = 1; i < nums.length; i++) {
+            // 이전 증가 방향가 같은 경우 swap
+            // nums[i] vs nums[i+1] when nums[i-1] <= nums[i] : nums[i] < nums[i+1] => swap(nums[i], nums[i+1])
+            if ((prevIncrease && nums[i - 1] < nums[i]) || (!prevIncrease && nums[i - 1] > nums[i])) {
+                temp = nums[i - 1];
+                nums[i - 1] = nums[i];
+                nums[i] = temp;
+            }
+
+            // 이전 증가 방향과 반대로 설정
+            prevIncrease = !prevIncrease;
         }
     }
 }
