409dbd93dcc2d0757ae9c392f7529c1cdd4d880d
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-16 18:01:03 +0900

    Time: 212 ms (39.65%), Space: 79.6 MB (72.37%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,43 +1,39 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int size = heights.length;
-        if (size == 1) return heights[0];
+        int numBoards = heights.length;
 
-        int ans = 0;
+        // 높이에 대한 오름차순 스택
+        Stack<Integer> monotonic = new Stack<>();
 
-        // 막대기 높이가 오름차순이 되도록 인덱스 저장
-        Stack<Integer> leftBoundIndex = new Stack<>();
+        int ans = heights[0];
 
-        // Width 계산 시, 스택의 2개 값을 이용
-        // top : 현재 높이, next top(i) : width 계산
-        // => width 는 i+1 ~ curIdx-1 까지의 판자로 구성
-        // => width = curIdx-1 - (i+1) +1 = curIdx-1-i
-        // 판자가 0부터시작하는 경우 i+1==0이 되므로, -1을 스택에 저장함
-        final int EMPTY_INDICATOR = -1;
-        leftBoundIndex.push(EMPTY_INDICATOR);
+        // heights[-1] 은 무한대라고 가정
+        monotonic.push(-1);
 
-        int curWidth, curHeight;
-        for (int i = 0; i < size; i++) {
-            // 현재 막대기보다 스택에 저장된 막대기 높이가 높은 경우
-            // => 현재 막대부터 새로운 문제가 시작됨 : 이전 막대들은 현재 막대 때문에 무조건 짤림
-            // => 현재 막대 이전에 있는 막대가 최소값인 경우의 최대값 계산
-            // 이때 i-1 번째까지의 막대기로만 사각형이 구성될 수 있음
-            while (leftBoundIndex.peek() != EMPTY_INDICATOR && heights[i] <= heights[leftBoundIndex.peek()]) {
-                // 현재 막대가 현재 계산하는 직사각형의 높이
-                curHeight = heights[leftBoundIndex.pop()];
+        int height, curHeight, curWidth;
+        for (int i = 0; i < numBoards; i++) {
+            height = heights[i];
 
-                // 스택에는 오름차순으로 저장되므로, 스택의 2번째 요소 바로 다음부터 사작형이 형성됨
-                curWidth = i - 1 - leftBoundIndex.peek();
+            // 현재 높이값 이상인 값들에 대해서 넓이 계산
+            // (why) 스택에 저장된 값들은 현재 막대기에 의해서 더이상 확장이 불가능
+            // 값이 같은 경우에도 현재 막대기에 의해 제한된다고 가정함 => 너비 계산에 대한 가정
+            while (monotonic.peek() != -1 && heights[monotonic.peek()] >= height) {
+                // 스택에 저장된 막대의 높이로 만들 수 있는 최대 직사각형
+                curHeight = heights[monotonic.pop()];
+
+                // 너비는 스택 다음에 저장된 막대기와 현재 막대기(i)에 의해 결정
+                curWidth = i - monotonic.peek() - 1;
                 ans = Math.max(ans, curHeight * curWidth);
             }
 
-            leftBoundIndex.push(i);
+            monotonic.push(i);
         }
 
-        // 스택 나머지 부분 계산
-        while (leftBoundIndex.peek() != EMPTY_INDICATOR) {
-            curHeight = heights[leftBoundIndex.pop()];
-            curWidth = size - 1 - leftBoundIndex.peek();
+        // 스택에 남이 있는 막대기로 직사각형 만들기
+        // => 로직 자체는 동일
+        while (monotonic.peek() != -1) {
+            curHeight = heights[monotonic.pop()];
+            curWidth = numBoards - monotonic.peek() - 1;
             ans = Math.max(ans, curHeight * curWidth);
         }
 
