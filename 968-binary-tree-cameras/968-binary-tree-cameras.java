c608916188f73744a1361dede272556b63d344a7
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-17 18:40:59 +0900

    Time: 2 ms (20.45%), Space: 41.7 MB (95.25%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -15,35 +15,38 @@
  */
 class Solution {
     private int ans;
-    private Set<TreeNode> isCovered;
+    private Set<TreeNode> covered;
 
     public int minCameraCover(TreeNode root) {
         ans = 0;
-        isCovered = new HashSet<>();
-        
-        // 자식 노드들이 널인 경우도 있으므로, 널 저장해 놓음
-        isCovered.add(null);
+        covered = new HashSet<>();
+
+        // 자식이 null 인 경우도 있으므로 null 을 추가해놓는다
+        covered.add(null);
+
         postOrder(root, null);
+
         return ans;
     }
 
     private void postOrder(TreeNode node, TreeNode parent) {
         if (node == null) return;
 
+        // post order : left -> right -> root
         postOrder(node.left, node);
         postOrder(node.right, node);
 
-        // 지금 노드에 설치해야 하는 경우
-        // 1. 부모 노드가 없는 경우, 자식 노드들이 이미 커버가 되어도 현재 노드에 카메라를 설치해야함
-        // 2. 부모 노드가 있는 경우에는, 자식 노드들 중 커버가 안된 노드가 있을 때만 커버
-        // => 자식 노드들이 커버가 된 경우에는 부모 노드에 설치하는게 이득
-        if (parent == null && !isCovered.contains(node) || !isCovered.contains(node.left) || !isCovered.contains(node.right)) {
-            isCovered.add(node);
-            isCovered.add(node.left);
-            isCovered.add(node.right);
-            isCovered.add(parent);
-
+        // 현재 노드에 카메라를 설치해야 하는 상황
+        // 루트 노드인 경우 : post order 이므로, 현재 노드가 커버되지 않은 경우 카메라 설치 필요
+        // 루트 노드가 아닌 경우 : 자식 노드들 중에 커버되지 않은 노드가 있는 경우
+        // => 전략 : 자식보다는 부모 노드 쪽에 설치
+        if ((parent == null && !covered.contains(node)) || (!covered.contains(node.left) || !covered.contains(node.right))) {
             ans++;
+            
+            covered.add(parent);
+            covered.add(node);
+            covered.add(node.left);
+            covered.add(node.right);
         }
     }
 }
