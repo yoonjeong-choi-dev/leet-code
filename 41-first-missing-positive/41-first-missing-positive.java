52ddc3911d30c8fe192e54c826e6a68efd5ca574
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-21 01:26:20 +0900

    Time: 6 ms (39.14%), Space: 124.1 MB (77.61%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,23 +1,35 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int len = nums.length;
+        
         final int NULL_VAL = len + 1;
-
-        for (int i = 0; i < len; i++) {
-            if (nums[i] <= 0 || nums[i] > len) nums[i] = NULL_VAL;
+        
+        // smallest missing positive integer
+        // => 음수 및 현재 길이보다 큰 숫자들은 신경쓰지 않는다
+        for(int i=0;i<len;i++){
+            if(nums[i] <= 0 || nums[i] > len) nums[i] = NULL_VAL;
         }
-
-        int curVal;
-        for (int i = 0; i < len; i++) {
+        
+        // expected : nums[i] = i+1
+        // => nums[i] < 0 : i+1 존재
+        int curIdx, curVal;
+        for(int i=0;i<len;i++){
             curVal = Math.abs(nums[i]);
-            if (curVal == NULL_VAL) continue;
-            if (nums[curVal - 1] > 0) nums[curVal - 1] = -nums[curVal - 1];
+            
+            // 불필요한 데이터인 경우 무시
+            if(curVal == NULL_VAL) continue;
+            
+            curIdx = curVal - 1;
+            if(nums[curIdx] > 0) nums[curIdx] *= -1;
         }
-
-        for (int i = 0; i < len; i++) {
-            if (nums[i] >= 0) return i+1;
+        
+        // 1 ~ len 까지 중에 없는 숫자 반환
+        // nums[i] >= 0 => return i+1
+        for(int i=0;i<len;i++){
+            if(nums[i] > 0) return i+1;
         }
-
-        return len + 1;
+        
+        // 1 ~len 까지 모두 존재
+        return len+1;
     }
 }
