4c3c863123d2fd9299617e123d3511d21fc77504
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-21 19:08:32 +0900

    Time: 75 ms (67.80%), Space: 116.2 MB (86.16%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,89 +1,99 @@
-    class Node {
-        int key;
-        int value;
-        Node prev;
-        Node next;
+class Node {
+    int key;
+    int val;
+    Node prev, next;
+    
+    Node() {
+        key = -1;
+        val = -1;
+    }
+    
+    Node(int key ,int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
 
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-            prev = null;
-            next = null;
+
+class LRUCache {
+    
+    private Map<Integer, Node> cache;
+    private Node head, tail;
+    private int capacity,size;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        
+        cache = new HashMap<>();
+        head = new Node();
+        tail = new Node();
+        
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        Node target = cache.getOrDefault(key, null);
+        if(target == null) return -1;
+        
+        updateCache(target);
+        return target.val;
+    }
+    
+    public void put(int key, int value) {
+        Node target = cache.getOrDefault(key, null);
+        
+        if(target == null) {
+            target = new Node(key ,value);
+            addCacheData(target);
+        } else {
+            target.val = value;
+            updateCache(target);
         }
     }
-
-    class LRUCache {
-
-        private final int capacity;
-        private final Map<Integer, Node> cache;
-        private final Node head, tail;
-
-        public LRUCache(int capacity) {
-            this.capacity = capacity;
-            cache = new HashMap<>();
-
-            head = new Node(-1, -1);
-            tail = new Node(-1, -1);
-
-            head.next = tail;
-            tail.prev = head;
-        }
-
-        public int get(int key) {
-            Node node = cache.getOrDefault(key, null);
-            if (node == null) return -1;
-
-            // 현재 노드를 가장 앞으로 이동
-            removeNode(node);
-            addFirst(node);
-
-            return node.value;
-        }
-
-        public void put(int key, int value) {
-            Node node = cache.getOrDefault(key, null);
-
-            if (node != null) {
-                // 이미 캐시에 있는 경우 => 값 및 캐시 업데이트
-                node.value = value;
-
-                // 캐시 업데이트
-                removeNode(node);
-                addFirst(node);
-            } else {
-                // 캐시에 없는 경우 => 노드 추가
-                node = new Node(key, value);
-
-                // 캐시 용량이 부족한 경우에는 가장 뒷부분 삭제
-                if (cache.size() == capacity) {
-                    // 캐시에서 가장 뒷 부분 삭제
-                    Node toDelete = tail.prev;
-                    removeNode(toDelete);
-
-                    cache.remove(toDelete.key);
-                }
-
-                // 노두 추가
-                cache.put(key, node);
-                addFirst(node);
-            }
-        }
-
-        private void removeNode(Node node) {
-            Node prev = node.prev;
-            Node next = node.next;
-            prev.next = next;
-            next.prev = prev;
-        }
-
-        private void addFirst(Node node) {
-            node.prev = head;
-            node.next = head.next;
-
-            head.next.prev = node;
-            head.next = node;
-        }
+    
+    private void addCacheData(Node node) {
+        
+        if(capacity == size) removeLastCache();
+        
+        // head -> node -> head.next
+        head.next.prev = node;
+        node.next = head.next;
+        
+        head.next = node;
+        node.prev = head;
+        
+        cache.put(node.key, node);
+        
+        size++;
     }
+    
+    private void updateCache(Node node) {
+        removeCacheData(node);
+        addCacheData(node);
+    }
+    
+    private void removeCacheData(Node node) {
+        // node.prev -> node.next
+        Node prev = node.prev;
+        Node next = node.next;
+        
+        prev.next = next;
+        next.prev = prev;
+        
+        cache.remove(node.key);
+        
+        size--;
+    }
+    
+    private void removeLastCache() {
+        Node toRemove = tail.prev;
+        if(head != toRemove) removeCacheData(toRemove);
+    }
+    
+}
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
