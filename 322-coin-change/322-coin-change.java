ff9dda2e84bb0eeb401e1b4187f223f87b1f477e
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-06-26 20:40:21 +0900

    Time: 24 ms (66.25%), Space: 45.1 MB (57.26%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,38 +1,44 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        // Edge Case
-        if (amount == 0) return 0;
         
-        // 작은 돈부터 고려
+        
+        // 낮은 코인부터 고려
         Arrays.sort(coins);
-
-        int[] dp = new int[amount + 1];
+        
+        // dp[i] : i원을 내기 위한 가장 작은 코인 갯수
+        int[] dp = new int[amount+1];
+        
+        // -1 로 초기화
         Arrays.fill(dp, -1);
-        for (int coin : coins) {
-            if(coin > amount) break;
-            dp[coin] = 1;
+        
+        // 코인 1개로 낼 수 있는 경우
+        for(int coin : coins) {
+            if(coin <= amount) dp[coin] = 1;
         }
-
-        // 각 돈에 대한 경우의 수 계산
-        for (int pay = 1; pay <= amount; pay++) {
-            if (dp[pay] != -1) continue;
-
-            for (int coin : coins) {
-                if (pay < coin) break;
-
-                // coin 을 내는 경우 경우의 수
-                if (dp[pay - coin] != -1) {
-                    if (dp[pay] == -1) {
-                        // 첫 발견
-                        dp[pay] = dp[pay - coin] + 1;
+        
+        // 0원을 낼 수 있는 경우는 0
+        dp[0] = 0;
+        
+        // 0원부터 탐색
+        for(int money=1;money<=amount;money++){
+            if(dp[money] != -1) continue;
+            
+            // money-coin 에 coin 추가
+            for(int coin : coins){
+                if(money < coin) break;
+                
+                if(dp[money-coin] != -1) {
+                    if(dp[money] == -1) {
+                        // 처음 발견한 정답
+                        dp[money] = dp[money-coin] + 1;
                     } else {
-                        // 이후 
-                        dp[pay] = Math.min(dp[pay], dp[pay - coin] + 1);
+                        // 두번째 이후 발견
+                        dp[money] = Math.min(dp[money], dp[money-coin] + 1);
                     }
                 }
             }
         }
-
+        
         return dp[amount];
     }
 }
