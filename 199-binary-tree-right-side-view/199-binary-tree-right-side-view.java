4326c7e6accb087d1d2e69f72c7ca835e010d84e
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-07-11 20:56:48 +0900

    Time: 1 ms (95.06%), Space: 43.2 MB (15.64%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -16,22 +16,23 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        if (root == null) return ans;
-
+        if(root == null) return ans;
+        
         Queue<TreeNode> bfs = new ArrayDeque<>();
         bfs.add(root);
-
+        
         TreeNode cur;
-        while (!bfs.isEmpty()) {
-            for (int i = bfs.size() - 1; i >= 0; i--) {
+        while(!bfs.isEmpty()) {
+            for(int i=bfs.size()-1;i>=0;i--) {
                 cur = bfs.poll();
-                if (cur.left != null) bfs.add(cur.left);
-                if (cur.right != null) bfs.add(cur.right);
-
-                if (i == 0) ans.add(cur.val);
+                
+                if(cur.left != null) bfs.add(cur.left);
+                if(cur.right != null) bfs.add(cur.right);
+                
+                if(i==0) ans.add(cur.val);
             }
         }
-
+        
         return ans;
     }
 }
