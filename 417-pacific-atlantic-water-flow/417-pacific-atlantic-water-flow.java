33e23073e5326c43279e1d401d0e54ab1e07d777
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-07-15 00:24:13 +0900

    Time: 7 ms (77.18%), Space: 55.2 MB (33.98%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,61 +1,60 @@
 class Solution {
-    private static final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    private int[][] heights;
+    
+    private static final int[][] dirs = {{0,1},{0,-1}, {1,0},{-1,0}};
+    
+    private int[][] heights, map;
     private int rowSize, colSize;
-    private int[][] result;
-
+    
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         this.heights = heights;
         rowSize = heights.length;
         colSize = heights[0].length;
-        result = new int[rowSize][colSize];
-
-        final int pacificFlag = 1;
-        final int atlanticFlag = 2;
-
+        map = new int[rowSize][colSize];
+        
         // Pacific
-        for (int i = 0; i < rowSize; i++) {
-            if (result[i][0] < pacificFlag) dfs(i, 0, pacificFlag);
+        final int flag1 = 1;
+        for(int i=0;i<rowSize;i++) {
+            if(map[i][0] < flag1) dfs(i,0,flag1);
         }
-        for (int i = 0; i < colSize; i++) {
-            if (result[0][i] < pacificFlag) dfs(0, i, pacificFlag);
+        for(int i=1;i<colSize;i++){
+            if(map[0][i] < flag1) dfs(0,i,flag1);
         }
-
+        
         // Atlantic
-        for (int i = 0; i < rowSize; i++) {
-            if (result[i][colSize - 1] < atlanticFlag) dfs(i, colSize - 1, atlanticFlag);
+        final int flag2 = 2;
+        int bottom = rowSize-1, right = colSize-1;
+        for(int i=0;i<rowSize;i++){
+            if(map[i][right] < flag2) dfs(i, right, flag2);
         }
-        for (int i = 0; i < colSize; i++) {
-            if (result[rowSize - 1][i] < atlanticFlag) dfs(rowSize - 1, i, atlanticFlag);
+        for(int i=0;i<right;i++){
+            if(map[bottom][i] < flag2) dfs(bottom, i, flag2);
         }
-
-        final int bothFlag = pacificFlag + atlanticFlag;
+        
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < rowSize; i++) {
-            for (int j = 0; j < colSize; j++) {
-                if (result[i][j] == bothFlag) {
-                    ans.add(Arrays.asList(i, j));
-                }
+        final int flag3 = flag1 + flag2;
+        for(int i=0;i<rowSize;i++){
+            for(int j=0;j<colSize;j++){
+                if(map[i][j] == flag3) ans.add(Arrays.asList(i,j));
             }
         }
         return ans;
     }
-
-    private void dfs(int x, int y, int flag) {
-        result[x][y] += flag;
-
+    
+    private void dfs(int x, int y, int color) {
+        // visit
+        map[x][y] += color;
+        
+        // dfs
+        int curHeight = heights[x][y];
         int nextX, nextY;
-        for (int[] d : directions) {
+        for(int[] d : dirs) {
             nextX = x + d[0];
             nextY = y + d[1];
-            if (nextX < 0 || nextX >= rowSize || nextY < 0 || nextY >= colSize) continue;
-
-            // 원래는 높은곳에서 낮거나 같은 곳으로 물이 흐름
-            // 바닷가에서 시작하여 반대 방향(낮은곳에서 높거나 같은 곳)으로 dfs 탐색하도록 변경
-            if (heights[x][y] <= heights[nextX][nextY] && result[nextX][nextY] < flag) {
-                dfs(nextX, nextY, flag);
-            }
+            
+            if(nextX < 0 || nextX>=rowSize || nextY<0 || nextY>=colSize) continue;
+            
+            // 현재 높이보다 높거나 같은 이웃 셀로 전파
+            if(heights[nextX][nextY] >= curHeight && map[nextX][nextY] < color) dfs(nextX, nextY, color);
         }
     }
 }
