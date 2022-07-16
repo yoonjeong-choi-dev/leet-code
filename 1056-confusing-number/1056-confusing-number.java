32099dfee2d9977dcb98ee379f982dae440f67e3
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-07-17 01:59:48 +0900

    Time: 4 ms (6.74%), Space: 40.9 MB (56.93%) - LeetHub

---
+++ 1056-confusing-number/1056-confusing-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    private static final Map<Integer, Integer> confused = new HashMap<Integer, Integer>() {{
+        put(0, 0);
+        put(1, 1);
+        put(6, 9);
+        put(8, 8);
+        put(9, 6);
+    }};
+    
+    public boolean confusingNumber(int n) {
+        // 0 <= n <= 10^ 9
+        List<Integer> digits = new ArrayList<>();
+        while (n != 0) {
+            digits.add(n % 10);
+            n /= 10;
+        }
+
+        int digitLen = digits.size();
+        int[] converted = new int[digitLen];
+        for (int i = 0; i < digitLen; i++) {
+            if (!confused.containsKey(digits.get(i))) return false;
+            converted[digitLen - 1 - i] = confused.get(digits.get(i));
+        }
+        
+        System.out.println(Arrays.toString(converted));
+
+        boolean isSame = true;
+        for (int i = 0; i < digits.size(); i++) {
+            if (!digits.get(i).equals(converted[i])) {
+                isSame = false;
+                break;
+            }
+        }
+
+        return !isSame;
+    }
+}
