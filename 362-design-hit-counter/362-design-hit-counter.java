81323ca7463bd7b3580807d6ed71fb645a125009
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-07-23 00:32:43 +0900

    Time: 2 ms (58.45%), Space: 41.8 MB (70.67%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.java
+++ 362-design-hit-counter/362-design-hit-counter.java
@@ -1,24 +1,35 @@
 class HitCounter {
 
         private static final int TERM = 300;
-        private final Queue<Integer> queue;
+        private int count;
+
+        // (timestamp, count)
+        // timestamp is monotonically increasing). Several hits may arrive roughly at the same time.
+        // => 함수 호출 시 큐의 첫 부분만 고려하면 됨
+        private final Queue<int[]> queue;
 
         public HitCounter() {
+            count = 0;
             queue = new ArrayDeque<>();
         }
 
         public void hit(int timestamp) {
-            queue.add(timestamp);
+            if (!queue.isEmpty() && queue.peek()[0] == timestamp) {
+                queue.peek()[1]++;
+            } else {
+                queue.add(new int[]{timestamp, 1});
+            }
+            count++;
         }
 
         public int getHits(int timestamp) {
             int lastTime = timestamp - TERM;
             while (!queue.isEmpty()) {
-                if (queue.peek() <= lastTime) queue.poll();
-                else break;
+                if (queue.peek()[0] <= lastTime) {
+                    count -= queue.poll()[1];
+                } else break;
             }
-
-            return queue.size();
+            return count;
         }
 }
 
