c3881f10b78e24eff1acc40d4086ffc43cc935db
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-07-28 22:16:09 +0900

    Time: 6 ms (63.45%), Space: 43.7 MB (61.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -4,17 +4,17 @@ class Solution {
         
         int len = s.length();
         
-        // consist of lowercase English letters
+        // s and t consist of lowercase English letters.
         int[] occurs = new int[26];
-        
         for(int i=0;i<len;i++){
-            occurs[s.charAt(i) - 'a']++;
-            occurs[t.charAt(i) - 'a']--;
+            occurs[s.charAt(i)-'a']++;
+            occurs[t.charAt(i)-'a']--;
         }
         
-        for(int occur : occurs) {
-            if(occur != 0) return false;
+        for(int n : occurs) {
+            if(n != 0) return false;
         }
+        
         return true;
     }
 }
