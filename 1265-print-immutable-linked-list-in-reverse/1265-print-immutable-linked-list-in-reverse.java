310ed97f4e067b6aafce13fc020fcf274503a698
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-07-31 00:12:28 +0900

    Time: 1 ms (96.93%), Space: 42.9 MB (23.99%) - LeetHub

---
+++ 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.java
@@ -0,0 +1,22 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * interface ImmutableListNode {
+ *     public void printValue(); // print the value of this node.
+ *     public ImmutableListNode getNext(); // return the next node.
+ * };
+ */
+
+class Solution {
+    public void printLinkedListInReverse(ImmutableListNode head) {
+        Stack<ImmutableListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.add(head);
+            head = head.getNext();
+        }
+
+        while (!stack.isEmpty()) {
+            stack.pop().printValue();
+        }
+    }
+}
