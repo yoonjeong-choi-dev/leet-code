17e408469a01b362a5fb04ac748dad86b89be6b3
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-01 15:34:41 +0900

    Time: 7 ms (63.12%), Space: 45.4 MB (12.42%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,41 +14,34 @@
  * }
  */
 class Solution {
+    
     private int ans, target;
-    private Map<Integer, Integer> distanceFromRoot;
-
+    private Map<Long, Integer> distanceFromRoot;
+    
     public int pathSum(TreeNode root, int targetSum) {
+        if(root == null) return 0;
+        
         ans = 0;
         target = targetSum;
         distanceFromRoot = new HashMap<>();
-
-        preOrder(root, 0);
-
+        
+        recur(root, 0);
         return ans;
     }
-
-    private void preOrder(TreeNode node, int curSum) {
-        if (node == null) return;
-
+    
+    private void recur(TreeNode node, long curSum) {
         curSum += node.val;
-
-        // root -> cur 경로가 정답인 경우
-        if (curSum == target) ans++;
-
-        // root -> something -> node 경로에서 root->something 경로 합이 curSum - target 인 something 노드가 있는 경우
-        // => something 다음 노드부터 현재 노드까지의 경로가 정답
+        if(curSum == target) ans++;
+        
+        // curSum - (조상 노드 거리) == target
         ans += distanceFromRoot.getOrDefault(curSum - target, 0);
-
-        // 현재 경로 합 저장
+        
+        // update distance
         distanceFromRoot.put(curSum, distanceFromRoot.getOrDefault(curSum, 0) + 1);
-
-        // 현재 경로에서 진행 가능한 왼쪽,오른쪽 서브트리 탐색
-        preOrder(node.left, curSum);
-        preOrder(node.right, curSum);
-
-        // 현재 경로가 끝나기 때문에 현재 경로 정보 삭제
-        // : preOrder 이기 때문에 현재 노드 하위 경로는 모두 탐색 완료했기 때문에
-        // i.e 이후 탐색하는 경로에는 현재 노드가 없음
-        distanceFromRoot.put(curSum, distanceFromRoot.get(curSum) - 1);
+        
+        if(node.left != null) recur(node.left, curSum);
+        if(node.right != null) recur(node.right, curSum);
+        
+        distanceFromRoot.put(curSum, distanceFromRoot.get(curSum) -1);
     }
 }
