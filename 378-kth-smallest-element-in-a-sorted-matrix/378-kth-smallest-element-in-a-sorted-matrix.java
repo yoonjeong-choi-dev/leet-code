e88ce9c71ec33d15de809675574dd05315445aac
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-02 20:59:55 +0900

    Time: 21 ms (54.59%), Space: 57.7 MB (16.28%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,28 +1,28 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int matSize = matrix.length;
-
-        PriorityQueue<int[]> rowPq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
+        int numRows = matrix.length;
+        int numCols = matrix.length;
+        
+        // (row, col)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
             public int compare(int[] o1, int[] o2) {
-                return Integer.compare(matrix[o1[0]][o1[1]], matrix[o2[0]][o2[1]]);
-            }
+                return Integer.compare(matrix[o1[0]][o1[1]],matrix[o2[0]][o2[1]]);
+            } 
         });
-
-        for (int i = 0; i < matSize; i++) {
-            rowPq.add(new int[]{i, 0});
-        }
-
-        int[] curRow ;
-        for (int i = 0; i < k - 1; i++) {
-            curRow = rowPq.poll();
-            curRow[1]++;
-
-            if (curRow[1] != matSize) {
-                rowPq.add(curRow);
+        
+        for(int i=0;i<numRows;i++) pq.add(new int[] {i, 0});
+        
+        int[] curCell;
+        while(true) {
+            curCell = pq.poll();
+            k--;
+            
+            if(k==0) {
+                return matrix[curCell[0]][curCell[1]];
             }
+            
+            curCell[1]++;
+            if(curCell[1] < numCols) pq.add(curCell);
         }
-        curRow = rowPq.poll();
-        return matrix[curRow[0]][curRow[1]];
     }
 }
