0ade8a7e0ffa9675929a1c55ebc546290169131b
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-07 01:07:29 +0900

    Time: 3 ms (92.57%), Space: 68.9 MB (59.08%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,28 +1,20 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int size = nums.length;
-        if (size <= k) return size;
-
-        int bound = k + 1;
-
-        // 0 들의 인덱스 저장 => bound 이상인 경우 앞의 인덱스부터 삭제해야 하므로 큐 사용
-        Queue<Integer> zeroIndices = new LinkedList<>();
-        int ans = k;
-
+        int len = nums.length;
+        
         int left = 0, right = 0;
-        while (right < size) {
-            if (nums[right] == 0) {
-                zeroIndices.offer(right);
+        int curZero = 0;
+        int ans = 0;
+        
+        while(right < len) {
+            if(nums[right++] == 0) curZero++;
+            
+            while(curZero > k) {
+                if(nums[left++] == 0) curZero--;
             }
-
-            if (zeroIndices.size() == bound) {
-                left = zeroIndices.poll() + 1;
-            }
-
-            right++;
-            ans = Math.max(ans, right - left);
+            
+            ans = Math.max(ans, right-left);
         }
-
         return ans;
     }
 }
