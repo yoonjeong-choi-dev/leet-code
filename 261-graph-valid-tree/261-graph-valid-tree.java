e6302db842bc943a656fcee06fd1b9dc55d34f6e
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-07 00:02:56 +0900

    Time: 8 ms (28.83%), Space: 48.3 MB (20.50%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,42 +1,33 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        // Make adjacent graph
-        List<List<Integer>> graph = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-
-        for (int[] e : edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<n;i++) graph.add(new ArrayList<>());
+        
+        for(int[] e : edges) {
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
-
-        boolean[] isVisited = new boolean[n];
-
-        // BFS : (node, parent)
+        
+        boolean[] visited = new boolean[n];
+        
+        // (node, parent)
         Queue<int[]> bfs = new ArrayDeque<>();
-        bfs.add(new int[]{0, -1});
-        isVisited[0] = true;
-
+        bfs.add(new int[]{0,-1});
+        visited[0] = true;
+        
         int[] cur;
-        while (!bfs.isEmpty()) {
+        while(!bfs.isEmpty()) {
             cur = bfs.poll();
-
-            for (int next : graph.get(cur[0])) {
-                // 부모 노드는 무시
-                if (next == cur[1]) continue;
-
-                // 사이클 형성
-                if (isVisited[next]) return false;
-
-                isVisited[next] = true;
+            for(int next : graph.get(cur[0])) {
+                if(next == cur[1]) continue;
+                
+                if(visited[next]) return false;
+                visited[next] = true;
                 bfs.add(new int[]{next, cur[0]});
             }
         }
-
-
-        // 컴포넌트가 1개인지 확인
-        for (int i = 0; i < n; i++) {
-            if (!isVisited[i]) return false;
-        }
+        
+        for(boolean v : visited) if(!v) return false;
         return true;
     }
 }
