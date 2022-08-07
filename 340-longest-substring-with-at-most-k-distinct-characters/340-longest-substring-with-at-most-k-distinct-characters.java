d53a28db13317f56f3e46b84d537168cf3edcb98
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-07 22:44:27 +0900

    Time: 19 ms (38.62%), Space: 45 MB (58.90%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
@@ -1,37 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if(k==0) return 0;
+        
         int len = s.length();
-
-        if (len <= k) return len;
-
-        int mapBound = k + 1;
-
-        // [0:i] 에서 가장 오른쪽 인덱스 저장
-        // 삭제 시, 가장 작은 인덱스를 삭제해야 하므로, 저장 순서가 보장되는 자료 구조 사용
-        Map<Character, Integer> rightest = new LinkedHashMap<>();
-
-        int left = 0, right = 0;
-        int ans = k;
-        char curChar;
-        int delIdx;
-        while (right < len) {
-            // 현재 문자를 포함했을 때 left 인덱스
-            curChar = s.charAt(right);
-            // 순서 보장을 위해 이미 있는 경우 삭제
-            if (rightest.containsKey(curChar)) rightest.remove(curChar);
-            rightest.put(curChar, right);
-
-            // 현재 문자 수가 k 초과 => 가장 왼쪽 문자 삭제
-            if (rightest.size() == mapBound) {
-                delIdx = rightest.values().iterator().next();
-                rightest.remove(s.charAt(delIdx));
-                left = delIdx + 1;
+        
+        Map<Character, Integer> counter = new HashMap<>();
+        
+        int left = 0, right = 0, prevCount;
+        char cur, prev;
+        
+        int ans = 1;
+        while(right < len) {
+            cur = s.charAt(right++);
+            counter.put(cur, counter.getOrDefault(cur, 0) + 1);
+            
+            while(counter.size() > k) {
+                prev = s.charAt(left++);
+                prevCount = counter.get(prev);
+                if(prevCount == 1) {
+                    counter.remove(prev);
+                } else {
+                    counter.put(prev, prevCount-1);
+                }
             }
-
-            right++;
+            
             ans = Math.max(ans, right - left);
         }
-
+        
         return ans;
     }
 }
