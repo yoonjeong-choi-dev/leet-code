904fede86e13907c94e4a7363c01571b8f9ef3f5
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-08 20:56:32 +0900

    Time: 1 ms (88.87%), Space: 41.7 MB (79.75%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,39 +1,52 @@
 class Solution {
+    // 2[a]2[b2[cd]]ef
+    // => ans : aa bcdcd bcdcd ef
+    
     public String decodeString(String s) {
         StringBuilder curAns = new StringBuilder();
-
-        Stack<StringBuilder> stringStack = new Stack<>();
+        
+        Stack<StringBuilder> sbStack = new Stack<>();
         Stack<Integer> countStack = new Stack<>();
-
-        StringBuilder curString;
-        char curChar;
-        int curCount = 0;
-        for (int i = 0; i < s.length(); i++) {
-            curChar = s.charAt(i);
-
-            if (Character.isDigit(curChar)) {
-                // 숫자가 나오면 (숫자들)[...] 형태가 나옴
-                curCount = curCount * 10 + curChar - '0';
-            } else if (curChar == '[') {
-                // 현재까지 숫자 저장 및 수 초기화
-                countStack.push(curCount);
-                curCount = 0;
-
-                // 현재까지 탐색한 문자열 저장 및 새로운 부분 문제를 위한 문자열 초기화
-                stringStack.push(curAns);
+        int curNum = 0;
+        
+        for(char c : s.toCharArray()) {
+            if(Character.isDigit(c)) {
+                curNum = curNum*10 + c - '0';
+            } else if(c == '[') {
+                // 반복할 숫자 저장
+                // 2[ : (2), ("")
+                // 2[a]2[ : (2), ("aa")
+                // 2[a]2[b2[ : (2,2), ("aa", "b")
+                countStack.push(curNum);
+                curNum = 0;
+                
+                // 현재까지 만들어진 문자열 저장
+                sbStack.push(curAns);
                 curAns = new StringBuilder();
-            } else if (curChar == ']') {
-                // 현재까지 만든 문자열을 n번 반복
-                curString = stringStack.pop();
-                for (int c = countStack.pop(); c > 0; c--) {
-                    curString.append(curAns);
+            } else if( c == ']') {
+                // [] 사이에 만들어진 문자열을 정답에 저장해야 함
+                // 2[a] : prevAns = "", curAns = "a" => curAns = "aa"
+                // 2[a]2[b2[cd] : (2,2), ("aa", "b") => prevAns = "b", curAns = "cd"
+                // => (2), ("aa"), curAns = bcdcd
+                // 2[a]2[b2[cd]] : (2), ("aa") => prevAns = ("aa") curAns = bcdcd
+                // => (),(), curAns = "aabcdcdbcdcd"
+                StringBuilder prevAns = sbStack.pop();
+                for(int i=countStack.pop();i>0;i--) {
+                    prevAns.append(curAns);
                 }
-                curAns = curString;
+                
+                curAns = prevAns;
             } else {
-                curAns.append(curChar);
+                // 현재 만들어지고 있는 문자열
+                // 2[a : curAns - "a"
+                // 2[a]2[b : curAns - "b"
+                // 2[a]2[b2[cd - curAns = "cd"
+                // 2[a]2[b2[cd]]ef - curAns = aabcdcdbcdcdef
+                curAns.append(c);
             }
         }
-
+        
+        
         return curAns.toString();
     }
 }
