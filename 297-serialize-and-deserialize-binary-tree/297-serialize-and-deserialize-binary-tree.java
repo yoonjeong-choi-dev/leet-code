85dc917789ce81f7280f1152e7a12fbef95a339e
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-08 20:20:02 +0900

    Time: 19 ms (66.73%), Space: 51.7 MB (67.42%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -8,48 +8,48 @@
  * }
  */
 public class Codec {
+    
+    private static final String NULL_SEP = "#";
+    private static final String NEXT_SEP = ",";
 
-    private static final String SEP_NULL = "#";
-        private static final String SEP_NEXT = ",";
-
-        // Encodes a tree to a single string.
-        public String serialize(TreeNode root) {
-            StringBuilder ans = new StringBuilder();
-            preorderSerialize(root, ans);
-            return ans.toString();
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        preorderSerialize(root, sb);
+        return sb.toString();
+    }
+    
+    private void preorderSerialize(TreeNode node, StringBuilder sb) {
+        if(node == null) {
+            sb.append(NULL_SEP).append(NEXT_SEP);
+            return;
         }
+        
+        sb.append(node.val).append(NEXT_SEP);
+        preorderSerialize(node.left, sb);
+        preorderSerialize(node.right, sb);
+    }
 
-        private void preorderSerialize(TreeNode node, StringBuilder sb) {
-            if (node == null) {
-                sb.append(SEP_NULL).append(SEP_NEXT);
-                return;
-            }
-
-            sb.append(String.valueOf(node.val)).append(SEP_NEXT);
-            preorderSerialize(node.left, sb);
-            preorderSerialize(node.right, sb);
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] tokens = data.split(NEXT_SEP);
+        LinkedList<String> nodeData = new LinkedList<>(Arrays.asList(tokens));
+        return inorderDeserialize(nodeData);
+    }
+    
+    private TreeNode inorderDeserialize(LinkedList<String> nodeData) {
+        String nodeVal = nodeData.getFirst();
+        nodeData.removeFirst();
+        
+        if(nodeVal.equals(NULL_SEP)) {
+            return null;
         }
-
-        // Decodes your encoded data to tree.
-        public TreeNode deserialize(String data) {
-            String[] tokens = data.split(SEP_NEXT);
-            LinkedList<String> nodeData = new LinkedList<>(Arrays.asList(tokens));
-
-            return preorderDeserialize(nodeData);
-        }
-
-        private TreeNode preorderDeserialize(LinkedList<String> list) {
-            String cur = list.get(0);
-            list.removeFirst();
-            if (cur.equals(SEP_NULL)) {
-                return null;
-            }
-
-            TreeNode node = new TreeNode(Integer.parseInt(cur));
-            node.left = preorderDeserialize(list);
-            node.right = preorderDeserialize(list);
-            return node;
-        }
+        
+        TreeNode node = new TreeNode(Integer.valueOf(nodeVal));
+        node.left = inorderDeserialize(nodeData);
+        node.right = inorderDeserialize(nodeData);
+        return node;
+    }
 }
 
 // Your Codec object will be instantiated and called as such:
