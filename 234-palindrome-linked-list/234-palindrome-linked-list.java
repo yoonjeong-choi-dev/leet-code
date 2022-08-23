b4a474cf71855c6f57a674d96b23b966f5429cb6
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-23 13:21:12 +0900

    Time: 3 ms (99.99%), Space: 59 MB (90.11%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,44 +10,37 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        // Assumption : size >=2
-        if (head == null || head.next == null) return true;
-
-        // Step 1 : 리스트를 두개로 나누기 => (n,n) or (n+1,n)
-        ListNode first = head, second = head;
-        while (second.next != null && second.next.next != null) {
+        if(head == null || head.next == null) return true;
+        
+        // 2n -> (n, n) 2n+1 -> (n+1, n)
+        ListNode first = head, second = head.next;
+        while(second != null && second.next != null) {
             first = first.next;
             second = second.next.next;
         }
+        
         second = first.next;
         first.next = null;
-        first = head;
-
-        // Step 2 : 두번째 리스트 반전
-        // prev(null) -> node1(cur) -> node2 -> node3 -> node4
-        // null <- node1(next prev), node2(next cur) -> node3 -> node4
-        // null <- node1 <- node2(next prev), node3(next cur) -> node4
-        ListNode prev = null, cur = second, next;
-        while (cur != null) {
-            // 다음 노드 임시 저장 : 현재 노드의 next 를 prev 노드에 연결하기 위해
-            next = cur.next;
-
-            // prev->cur 을 prev<-cur 로 변경
+        
+        // reverse second
+        ListNode prev = null, cur = second, temp;
+        while(cur!=null) {
+            // prev -> cur => prev <- cur
+            temp = cur.next;
             cur.next = prev;
-
-            // prev 및 cur 노드 변경
+            
             prev = cur;
-            cur = next;
+            cur = temp;
         }
+        
+        first = head;
         second = prev;
-
-        // compare first and second
-        while (second != null) {
-            if (first.val != second.val) return false;
+        
+        while(second != null) {
+            if(first.val != second.val) return false;
             first = first.next;
             second = second.next;
         }
-
         return true;
     }
 }
