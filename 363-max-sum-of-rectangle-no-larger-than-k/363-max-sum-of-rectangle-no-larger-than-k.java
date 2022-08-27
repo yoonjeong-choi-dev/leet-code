7c8280dfe6d4448065b5ce7f2fe47faf77a3849c
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-27 16:36:18 +0900

    Time: 698 ms (46.25%), Space: 116 MB (61.61%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
@@ -1,56 +1,54 @@
 class Solution {
-    
-    private int ans,target;
+    private int ans, target;
     private int[] partialSum;
-    
+
     public int maxSumSubmatrix(int[][] matrix, int k) {
         int rowSize = matrix.length, colSize = matrix[0].length;
-        
         ans = Integer.MIN_VALUE;
         target = k;
-        
-        // partialSum[j] : matrix[i:row][0:j] 합
+
+        // matrix[start:i] 부분 행렬에 대한 부분 합
+        // partialSum[j] = matrix[start:i][0:j] 부분 합
         partialSum = new int[colSize];
-        
-        
-        // matrix[i:][:] 부분 행렬에 대한 정답 찾기
-        for(int i=0;i<rowSize;i++){
+
+        // matrix[startRow:] 에 대한 부분 문제
+        for (int startRow = 0; startRow < rowSize; startRow++) {
             Arrays.fill(partialSum, 0);
-            
-            // matrix[i:row] 누적으로 탐색
-            for(int row=i;row<rowSize;row++){
-                // partialSum for row-th row
-                for(int col=0;col<colSize;col++) partialSum[col] += matrix[row][col];
-                
-                // matrix[i:row] 부분 행렬에 대해서 target 합이 있는지 확인
+
+            for (int row = startRow; row < rowSize; row++) {
+                // matrix[startRow:row] 부분 행렬에 대해서 탐색
+                for (int col = 0; col < colSize; col++) partialSum[col] += matrix[row][col];
+
+                // matrix[startRow:row] 에서 정답 찾기
+                // <=> Find i<j s.t partialSum[j] - partialSum[i] <= k
                 findTarget();
-                
-                // 조기종료 
-                if(ans == target) return ans;
+
+                // 정답은 target 보다 클 수 없음 => 조기 종료
+                if (ans == target) return ans;
             }
         }
-        
+
         return ans;
     }
-    
+
     private void findTarget() {
-        int sum = 0;
-        TreeSet<Integer> sorted = new TreeSet<>();
-        sorted.add(0);
+        // 빠른 탐색을 위해 BST 사용
+        TreeSet<Integer> sortedPartialSum = new TreeSet<>();
         
-        // 1 차원 부분합 배열을 이용하여 특정 범위의 합을 찾는 과정
-        // => target 이하의 합을 찾아야 함
-        // i.e target = partial[i] - partial[j] for some j < i
-        for(int num : partialSum) {
-            sum += num;
-            
-            // target = sum - x  <=> x = sum - target
-            Integer x = sorted.ceiling(sum - target);
-            
-            // update answer
-            if(x!=null) ans = Math.max(ans, sum - x);
-            
-            sorted.add(sum);
+        // 단일 요소일 수도 있으므로 0 추가
+        sortedPartialSum.add(0);
+
+        int curSum = 0;
+        for (int num : partialSum) {
+            curSum += num;
+
+            // Find i<j s.t partialSum[j] - partialSum[i] <= k
+            // <=> Find smallest x in sortedPartialSum s.t sum - x <= k i.e sum-k <= x
+            Integer x = sortedPartialSum.ceiling(curSum - target);
+
+            if (x != null) ans = Math.max(ans, curSum - x);
+
+            sortedPartialSum.add(curSum);
         }
     }
 }
