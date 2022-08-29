49973082a9c5535a0a3f04ac789c8e86f60b4be4
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-08-29 21:08:58 +0900

    Time: 3 ms (97.77%), Space: 58.2 MB (11.59%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,37 +1,34 @@
 class Solution {
-    private static final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
     private char[][] grid;
-    private int rowSize, colSize;
-    private boolean[][] isVisited;
-
+    int rowSize, colSize;
+    
     public int numIslands(char[][] grid) {
         this.grid = grid;
         rowSize = grid.length;
         colSize = grid[0].length;
-        isVisited = new boolean[rowSize][colSize];
-
-        int numComponent = 0;
-        for (int i = 0; i < rowSize; i++) {
-            for (int j = 0; j < colSize; j++) {
-                if (grid[i][j] == '1' && !isVisited[i][j]) {
-                    numComponent++;
-                    dfs(i, j);
+        
+        int ans = 0;
+        for(int i=0;i<rowSize;i++){
+            for(int j=0;j<colSize;j++){
+                if(grid[i][j] == '1') {
+                    ans++;
+                    dfs(i,j);
                 }
             }
         }
-        return numComponent;
+        
+        return ans;
     }
-
+    
     private void dfs(int x, int y) {
-        isVisited[x][y] = true;
-
-        int nextX, nextY;
-        for (int[] d : directions) {
-            nextX = x + d[0];
-            nextY = y + d[1];
-
-            if (nextX < 0 || nextX >= rowSize || nextY < 0 || nextY >= colSize) continue;
-            if (grid[nextX][nextY] == '1' && !isVisited[nextX][nextY]) dfs(nextX, nextY);
-        }
+        if(x<0 || x >= rowSize || y < 0 || y >= colSize) return;
+        if(grid[x][y] == '0') return;
+        
+        grid[x][y] = '0';
+        dfs(x+1, y);
+        dfs(x-1, y);
+        dfs(x, y+1);
+        dfs(x, y-1);
     }
 }
