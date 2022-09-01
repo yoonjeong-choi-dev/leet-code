bdbb5f5e4754b7303955b6044cce55e593de4899
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-09-02 00:14:50 +0900

    Time: 8 ms (68.53%), Space: 52 MB (94.41%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.java
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.java
@@ -1,28 +1,25 @@
 class Solution {
     public int minSwaps(int[] data) {
+        int len = data.length;
         int numOnes = 0;
-        for (int d : data) {
-            if (d == 1) numOnes++;
+        for(int n : data) {
+            if(n == 1) numOnes++;
         }
-
-        if (numOnes <= 1 || numOnes == data.length) return 0;
-
-        // numOnes 길이의 윈도우에 가장 많은 1이 들어 있어야 함
-        // => 나머지 0들을 윈도우 밖의 1과 교환
+        
+        if(numOnes < 2 || numOnes == len) return 0;
+        
         int curZeros = 0;
-        for (int i = 0; i < numOnes; i++) {
-            if (data[i] == 0) curZeros++;
+        for(int i=0;i<numOnes;i++){
+            if(data[i] == 0) curZeros++;
         }
-
+        
         int ans = curZeros;
-        for (int i = numOnes; i < data.length; i++) {
-            // i 추가, i-numOnes 제거
-            if (data[i - numOnes] == 0) curZeros--;
-            if (data[i] == 0) curZeros++;
-
+        for(int i=numOnes;i<len;i++){
+            if(data[i] == 0) curZeros++;
+            if(data[i-numOnes] == 0) curZeros--;
+            
             ans = Math.min(ans, curZeros);
         }
-
         return ans;
     }
 }
