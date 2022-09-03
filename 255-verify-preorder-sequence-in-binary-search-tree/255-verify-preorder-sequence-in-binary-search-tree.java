8af9fab7b6cd5ca462f0e8dfefbe23f1d1c327f9
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-09-04 00:32:39 +0900

    Time: 255 ms (27.79%), Space: 53.9 MB (53.35%) - LeetHub

--- 255-verify-preorder-sequence-in-binary-search-tree/255-verify-preorder-sequence-in-binary-search-tree.java
+++ 255-verify-preorder-sequence-in-binary-search-tree/255-verify-preorder-sequence-in-binary-search-tree.java
@@ -1,35 +1,27 @@
 class Solution {
+    
     private int[] preorder;
-
+    
     public boolean verifyPreorder(int[] preorder) {
-        // pre : root -> left -> right
         this.preorder = preorder;
-        return recur(0, preorder.length - 1, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        return recur(0, preorder.length-1, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
-
-    private boolean recur(int start, int end, int upper, int lower) {
-        // null node
+    
+    
+    private boolean recur(int start, int end, int lowerBound, int upperBound) {
         if(start > end) return true;
         
-        // lower < root < upper
-        int root = preorder[start];
-        if (root <= lower || root >= upper) return false;
-
-        // leaf node
-        if (start == end) return true;
-
-        // [start], [start+1, rightStart-1], [rightStart, end]
-        int rightStart = start + 1;
-        for (; rightStart <= end; rightStart++) {
-            if (preorder[rightStart] > root) break;
-        }
-
-        // left tree check
-        if (!recur(start + 1, rightStart - 1, root, lower)) return false;
-
-        // right tree check
-        if (!recur(rightStart, end, upper, root)) return false;
-
+        int curVal = preorder[start];
+        if(curVal <= lowerBound || curVal >= upperBound) return false;
+        
+        // [start+1, rightStart=1], [rightStart, end]
+        int rightStart = start+1;
+        
+        // All the elements of preorder are unique.
+        while(rightStart <= end && preorder[rightStart] < curVal) rightStart++;
+        
+        if(!recur(start+1, rightStart-1, lowerBound, curVal)) return false;
+        if(!recur(rightStart, end, curVal, upperBound)) return false;
         return true;
     }
 }
