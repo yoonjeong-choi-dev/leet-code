0b3da75d9530a786b23815fd63cbbfc3a4e80840
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-09-04 21:51:45 +0900

    Time: 12 ms (92.72%), Space: 51.2 MB (90.84%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,55 +10,53 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        return mergeSort(head);
+        return sort(head);
     }
-
-    private ListNode mergeSort(ListNode node) {
-        // 사이즈가 1 이하면 정렬 완료
-        if (node == null || node.next == null) return node;
-
-        // 분할 정복을 위한 분할
-        ListNode mid = getMid(node);
-
-        // 분할된 문제 풀기
-        ListNode left = mergeSort(node);
-        ListNode right = mergeSort(mid);
+    
+    private ListNode sort(ListNode node) {
+        // sort complete when len is 0 or 1
+        if(node == null || node.next == null) return node;
+        
+        ListNode mid = divide(node);
+        
+        ListNode left = sort(node);
+        ListNode right = sort(mid);
+        
         return merge(left, right);
     }
-
+    
     private ListNode merge(ListNode left, ListNode right) {
-        ListNode ans = new ListNode();
-        ListNode cur = ans;
-
-        while (left != null && right != null) {
-            if (left.val < right.val) {
+        ListNode ret = new ListNode(-1);
+        ListNode cur = ret;
+        
+        while(left != null && right != null) {
+            if(left.val < right.val) {
                 cur.next = left;
                 left = left.next;
             } else {
                 cur.next = right;
                 right = right.next;
             }
+            
             cur = cur.next;
         }
-
-        cur.next = (left != null) ? left : right;
-
-        return ans.next;
+        
+        cur.next = left == null ? right : left;
+        return ret.next;
     }
-
-    private ListNode getMid(ListNode node) {
-
-        // Assumption : 노드 길이는 2이상
-        ListNode prev = null;
-        while (node != null && node.next != null) {
-            prev = (prev == null) ? node : prev.next;
-            node = node.next.next;
+    
+    
+    private ListNode divide(ListNode node) {
+        if(node == null || node.next == null) return node;
+        
+        ListNode right = node.next;
+        while(right != null && right.next != null) {
+            node = node.next;
+            right = right.next.next;
         }
-
-        // 노드를 node->prev, ret(prev.next) 로 쪼갬
-        // => prev.next 를 널로 만들어서 쪼갬
-        ListNode ret = prev.next;
-        prev.next = null;
-        return ret;
+        
+        right = node.next;
+        node.next = null;
+        return right;
     }
 }
