d465c7a2d37fd3e0b9d5fc89fc207fdeb062f922
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-09-12 02:43:25 +0900

    Time: 6 ms (49.39%), Space: 50.2 MB (37.36%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,18 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        int targetCount = nums.length / 3 + 1;
+        // Idea : n/3 + 1 이상 출현하는 숫자는 최대 2개
+        // => 최대 출현하는 2개의 숫자만 추적하면 된다
+        final int NULL_VALUE = Integer.MIN_VALUE;
+        int candidate1 = NULL_VALUE, candidate2 = NULL_VALUE;
+        int score1 = 0, score2 = 0;
 
-        HashMap<Integer, Integer> counter = new HashMap<>();
-        int curCount;
         for (int num : nums) {
-            curCount = counter.getOrDefault(num, 0);
-            if (curCount == targetCount) continue;
-
-            counter.put(num, curCount + 1);
-            if (counter.get(num) == targetCount) ans.add(num);
+            // 이미 추적 중인 후보인 경우 있으면 점수 증가
+            if (candidate1 == num) score1++;
+            else if (candidate2 == num) score2++;
+                // 추적하는 후보가 없으면, 후보 및 점수 등록
+            else if (score1 == 0) {
+                candidate1 = num;
+                score1 = 1;
+            } else if (score2 == 0) {
+                candidate2 = num;
+                score2 = 1;
+                // 추적 중인 후보와 다른 경우에는 점수 감소
+            } else {
+                score1--;
+                score2--;
+            }
         }
-        
+
+        // 최고 점수 2개에 대한 출현 횟수
+        int count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int targetCount = nums.length / 3;
+        if (count1 > targetCount) ans.add(candidate1);
+        if (count2 > targetCount) ans.add(candidate2);
         return ans;
     }
 }
