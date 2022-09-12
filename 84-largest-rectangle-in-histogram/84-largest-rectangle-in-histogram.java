2771a53387fb2382528f38372238b2d4c0cdf061
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-09-12 23:52:27 +0900

    Time: 218 ms (40.34%), Space: 87.9 MB (60.45%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,42 +1,39 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int numBoards = heights.length;
-
-        // 높이에 대한 오름차순 스택
-        Stack<Integer> monotonic = new Stack<>();
-
+        int totalWidth = heights.length;
         int ans = heights[0];
-
-        // heights[-1] 은 무한대라고 가정
-        monotonic.push(-1);
-
-        int height, curHeight, curWidth;
-        for (int i = 0; i < numBoards; i++) {
+        
+        // 낮은 높이를 만나면, 다음 높이들은 현재 낮은 높이에 의해 제한됨
+        // => 이후 높이들을 이용하는 경우, 앞에 있던 낮은 높이에 의해 제한
+        // 높이에 대한 오름차순으로 인덱스 저장
+        Stack<Integer> monoStack = new Stack<>();
+        
+        // 0번 부터 사용하는 것을 고려하기 위해 -1 인덱스 저장
+        monoStack.push(-1);
+        
+        int curWidth, curHeight, height;
+        for(int i=0;i<totalWidth;i++){
             height = heights[i];
-
-            // 현재 높이값 이상인 값들에 대해서 넓이 계산
-            // (why) 스택에 저장된 값들은 현재 막대기에 의해서 더이상 확장이 불가능
-            // 값이 같은 경우에도 현재 막대기에 의해 제한된다고 가정함 => 너비 계산에 대한 가정
-            while (monotonic.peek() != -1 && heights[monotonic.peek()] >= height) {
-                // 스택에 저장된 막대의 높이로 만들 수 있는 최대 직사각형
-                curHeight = heights[monotonic.pop()];
-
-                // 너비는 스택 다음에 저장된 막대기와 현재 막대기(i)에 의해 결정
-                curWidth = i - monotonic.peek() - 1;
-                ans = Math.max(ans, curHeight * curWidth);
+            
+            // 현재 높이보다 높은 막대기들의 높이를 가지는 직사각형 계산 : 이후에는 스택에 있는 막대기 전체 사용이 불가능 하므로
+            while(monoStack.peek() != -1 && heights[monoStack.peek()] >= height) {
+                curHeight = heights[monoStack.pop()];
+                
+                // 모노 스택이므로, monoStack.peek() 과 i 사이에 값들은 curHeight 보다 크거가 같음
+                // ex) Example1 - i== 4
+                curWidth = i - monoStack.peek() -1;
+                ans = Math.max(ans, curWidth*curHeight);
             }
-
-            monotonic.push(i);
+            
+            monoStack.push(i);
         }
-
-        // 스택에 남이 있는 막대기로 직사각형 만들기
-        // => 로직 자체는 동일
-        while (monotonic.peek() != -1) {
-            curHeight = heights[monotonic.pop()];
-            curWidth = numBoards - monotonic.peek() - 1;
-            ans = Math.max(ans, curHeight * curWidth);
+        
+        // 스택에 있는 나머지 높이들에 대해서도 계산
+        while(monoStack.peek() != -1) {
+            curHeight = heights[monoStack.pop()];
+            curWidth = totalWidth - monoStack.peek() -1;
+            ans = Math.max(ans, curWidth*curHeight);
         }
-
         return ans;
     }
 }
