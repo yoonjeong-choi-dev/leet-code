9ad36220c5c460ed3a35051889c3bf7afed1302a
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-09-12 19:42:09 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (55.87%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,22 +2,19 @@ class Solution {
     public int rob(int[] nums) {
         int numHouse = nums.length;
         
-        // Assumption : numHouse > 1
         if(numHouse == 1) return nums[0];
         
-        // [0,i] 구간까지 부분 정답
         int[] dp = new int[numHouse];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
         
         int ans = Math.max(dp[0], dp[1]);
         
-        for(int i=2;i<numHouse;i++){
-            // i번째 집 훔칠지 말지 결정
-            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
+        for(int i=2;i<numHouse;i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
             ans = Math.max(ans, dp[i]);
         }
-        
+            
         return ans;
     }
 }
