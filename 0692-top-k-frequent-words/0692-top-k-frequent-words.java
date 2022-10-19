d404d46cda447af936890514aeefb931043fd7af
Yoonjeong Choi <58597574+yoonjeong-choi-dev@users.noreply.github.com>
2022-10-19 19:38:54 +0900

    Time: 14 ms (35.97%), Space: 45.4 MB (32.96%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,26 +1,25 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String, Integer> occurs = new HashMap<>();
-        for(String word : words) occurs.put(word, occurs.getOrDefault(word, 0) + 1);
+        Map<String, Integer> counters = new HashMap<>();
+        for(String word : words) counters.put(word, counters.getOrDefault(word, 0) + 1);
         
-        PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>(){
-           public int compare(String s1, String s2) {
-               if(occurs.get(s1) != occurs.get(s2)) {
-                   return occurs.get(s1) - occurs.get(s2);
-               } else {
-                   return s2.compareTo(s1);
-               }
-           } 
+        PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<>(){
+            public int compare(String s1, String s2) {
+                if(counters.get(s1) == counters.get(s2)) {
+                    return s2.compareTo(s1);
+                } else {
+                    return counters.get(s1) - counters.get(s2);
+                }
+            }
         });
         
-        for(String key : occurs.keySet()) {
+        for(String key : counters.keySet()) {
             pq.add(key);
             if(pq.size() > k) pq.poll();
         }
         
         LinkedList<String> ans = new LinkedList<>();
         while(!pq.isEmpty()) ans.addFirst(pq.poll());
-        
         return ans;
     }
 }
